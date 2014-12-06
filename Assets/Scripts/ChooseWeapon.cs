8145d48d4aa6991c696e91937830c4f1f2eac45d
Ryan RC <murphyslaw480@gmail.com>
2014-12-05 22:43:39 -0500

    add some more weapon and material data

---
+++ Assets/Scripts/ChooseWeapon.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Linq;
+
+public class ChooseWeapon : MonoBehaviour {
+    public int width, height;
+    public string selection { get; private set; }
+    private string[] _options;
+
+    void OnGUI() {
+        var area = new Rect(transform.position.x, transform.position.y, width, height);
+        int idx = GUI.SelectionGrid(area, 0, _options, 1);
+        selection = _options[idx];
+    }
+
+    // Use this for initialization
+    void Start() {
+        _options = DataManager.FetchAll<WeaponData>().Select(x => x.name).ToArray();
+    }
+}
