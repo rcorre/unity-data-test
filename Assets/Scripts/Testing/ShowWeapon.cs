077ab25e1e22b5b106d885bc43080e0f7b3a7fa2
Ryan RC <murphyslaw480@gmail.com>
2014-12-05 23:02:55 -0500

    show weapon info

--- Assets/Scripts/Testing/ShowWeapon.cs
+++ Assets/Scripts/Testing/ShowWeapon.cs
@@ -15,5 +15,25 @@ public class ShowWeapon : MonoBehaviour {
 
         var text = GetComponent<Text>();
         text.text = "Name: " + weapon.ToString();
+        text.text += "\nValue: " + weapon.value;
+        text.text += "\nWeight: " + weapon.weight;
+        text.text += "\nElement: " + weapon.element;
+        text.text += "\nAttackType: " + weapon.attackType;
+        text.text += "\nDamage: " + weapon.damage.ToString();
+        text.text += "\nAccuracy: " + weapon.accuracy;
+        text.text += "\nAC[melee]: " + weapon.armorClass[AttackType.Melee];
+        text.text += "\nAC[ranged]: " + weapon.armorClass[AttackType.Ranged];
+        text.text += "\nAC[magic]: " + weapon.armorClass[AttackType.Magic];
+        text.text += "\nAC[magic]: " + weapon.armorClass[AttackType.Magic];
+
+        text.text += "\nResist[Slash]: " + weapon.resistance[Element.Slash];
+        text.text += "\nResist[Crush]: " + weapon.resistance[Element.Crush];
+        text.text += "\nResist[Pierce]: " + weapon.resistance[Element.Pierce];
+        text.text += "\nResist[Fire]: " + weapon.resistance[Element.Fire];
+        text.text += "\nResist[Ice]: " + weapon.resistance[Element.Ice];
+        text.text += "\nResist[Electric]: " + weapon.resistance[Element.Electric];
+        text.text += "\nResist[Earth]: " + weapon.resistance[Element.Earth];
+        text.text += "\nResist[Dark]: " + weapon.resistance[Element.Dark];
+        text.text += "\nResist[Light]: " + weapon.resistance[Element.Light];
     }
 }
